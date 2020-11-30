c2e48fdf4bb100a7b620986629ec5fd467922a39
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2020-11-30 23:31:52 +0700

    Update LashGame.java

--- LashGame.java
+++ LashGame.java
@@ -1,1 +1,23 @@
 package gameTetris;
+
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import javax.swing.JWindow;
+
+public class LashGame extends JWindow {
+   public Image LoadImage(String image){ 
+		try{ 
+			File file = new File(image);
+			InputStream is = new FileInputStream(file);
+			Image im = ImageIO.read(new BufferedInputStream(is));
+			return im;
+		}
+    catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
